13cb1c832bfae36fe277897ce77d2ca48f33b249
carlpd <89635997+carlpd@users.noreply.github.com>
2024-11-14 12:03:49 +0100

    Uploaded PHYSLITE folder files except subfolders

---
+++ PHYSLITE/AutoDict_ROOT__VecOps__RVec_vector_float___cxx.d
@@ -0,0 +1,36 @@
+
+# DO NOT DELETE
+
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/ROOT/RVec.hxx
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/Rtypes.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/RtypesCore.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/ROOT/RConfig.hxx
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/RVersion.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/ROOT/RVersion.hxx
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/RConfigure.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/DllImport.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/strtok.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/strlcpy.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/snprintf.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/TGenericClassInfo.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/TSchemaHelper.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/TIsAProxy.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/TVirtualIsAProxy.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/TError.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/vdtMath.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/sin.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/sincos.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/vdtcore_common.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/asin.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/cos.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/tan.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/tanh.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/atan.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/atan2.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/exp.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/log.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/sqrt.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/inv.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/vdt/identity.h
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx.so: /storage/shared/root_install/include/RVersion.h /storage/shared/root_install/include/ROOT/RConfig.hxx /storage/shared/root_install/include/TClass.h /storage/shared/root_install/include/TDictAttributeMap.h /storage/shared/root_install/include/TInterpreter.h /storage/shared/root_install/include/TROOT.h /storage/shared/root_install/include/TBuffer.h /storage/shared/root_install/include/TMemberInspector.h /storage/shared/root_install/include/TError.h /storage/shared/root_install/include/RtypesImp.h /storage/shared/root_install/include/TIsAProxy.h /storage/shared/root_install/include/TFileMergeInfo.h /storage/shared/root_install/include/TCollectionProxyInfo.h /storage/shared/root_install/bin/rootcling
+AutoDict_ROOT__VecOps__RVec_vector_float___cxx__ROOTBUILDVERSION= 6.30/04
